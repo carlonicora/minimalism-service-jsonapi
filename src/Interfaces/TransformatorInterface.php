6a35fab309d4735c1ecf8fbe132ae159f03fee27
Carlo Nicora <carlo.nicora@gmail.com>
2021-01-05 14:29:23 +0100

    construct removed from TransformatorInterface

--- src/Interfaces/TransformatorInterface.php
+++ src/Interfaces/TransformatorInterface.php
@@ -4,11 +4,6 @@ namespace CarloNicora\Minimalism\Services\JsonApi\Interfaces;
 interface TransformatorInterface
 {
     /**
-     * TransformationsInterface constructor.
-     */
-    public function __construct();
-
-    /**
      * @param string $transformationFunction
      * @param array $data
      * @param string|null $fieldName
